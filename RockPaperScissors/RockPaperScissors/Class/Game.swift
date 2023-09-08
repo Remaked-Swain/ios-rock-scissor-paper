3fd78272fe20fb0959ad1cd0448a800f5a641266
YunSwain <destap@naver.com>
2023-09-09 01:55:33 +0900

    [Feat] 묵찌빠 게임을 구현할 중심 로직: Game class 와 관련 메서드 중 사용자로부터 입력 받는 기능 추가

--- RockPaperScissors/RockPaperScissors/Class/Game.swift
+++ RockPaperScissors/RockPaperScissors/Class/Game.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+final class Game {
+    func run() {
+        
+    }
+    
+    func doRSP() {
+        
+    }
+}
+
+// MARK: Resuable Methods
+extension Game {
+    private func input() -> InputType {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+        
+        guard let input = readLine(), let intInput = Int(input) else { return .invalidInput }
+        
+        switch intInput {
+        case 0: return .exitProgram
+        case 1, 2, 3: return .handSign
+        default: return .invalidInput
+        }
+    }
+    
+    private func determineWinner(computerHandSign: HandSign, userHandSign: HandSign) -> PlayerType {
+        
+    }
+}

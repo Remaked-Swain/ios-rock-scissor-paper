30b264f40eb9e2a0c19c343d16b43e91a377c487
YunSwain <destap@naver.com>
2023-09-10 00:42:30 +0900

    [Docs, Refactor] README.md 변경, 일부 스크립트 지정된 조건 변경

--- RockPaperScissors/RockPaperScissors/Namespace/Script.swift
+++ RockPaperScissors/RockPaperScissors/Namespace/Script.swift
@@ -10,9 +10,9 @@ import Foundation
 @frozen enum Script: CustomStringConvertible {
     case input(_ currentWinner: PlayerType)
     case invalidInput
-    case win(_ currentWinner: PlayerType)
+    case win
     case lose
-    case draw
+    case draw(_ currentWinner: PlayerType)
     case noticeCurrentWinner(_ currentWinner: PlayerType)
     case gameOver
     
@@ -22,12 +22,12 @@ import Foundation
             return currentWinner == .none ? "가위(1), 바위(2), 보(3)! <종료 : 0> : " : "[\(currentWinner) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : "
         case .invalidInput:
             return "잘못된 입력입니다. 다시 시도해주세요."
-        case .win(let currentWinner):
-            return currentWinner == .none ? "이겼습니다!" : "\(currentWinner)의 승리!"
+        case .win:
+            return "이겼습니다!"
         case .lose:
             return "졌습니다!"
-        case .draw:
-            return "비겼습니다!"
+        case .draw(let currentWinner):
+            return currentWinner == .none ? "비겼습니다!" : "\(currentWinner)의 승리!"
         case .noticeCurrentWinner(let currentWinner):
             return "\(currentWinner)의 턴입니다."
         case .gameOver:
